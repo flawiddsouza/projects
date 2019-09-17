d492f1210c35f5a0d6badd30cd8acf1ef98adc97
Flawid D'Souza <me@flawiddsouza.com>
2019-09-17 14:11:11 +0530

    Adds View Task

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,10 @@ export default function Index() {
     const [ projectMembers, setProjectMembers ] = useState([])
     const [ tasks, setTasks ] = useState([])
     const [ currentProjectSlug, setCurrentProjectSlug ] = useState(null)
-    const [ showAddTaskModal, setshowAddTaskModal ] = useState(false)
+    const [ showAddTaskModal, setShowAddTaskModal ] = useState(false)
+    const [ showViewTaskModal, setShowViewTaskModal ] = useState(false)
+    const [ task, setTask ] = useState(null)
+    const [ activeTab, setActiveTab ] = useState('comments')
 
     let addTaskObj = {
         date: format(new Date, 'yyyy-MM-dd'),
@@ -153,7 +156,7 @@ export default function Index() {
 
     function handleAddTaskKeydown(e) {
         if(e.key === 'Escape') {
-            setshowAddTaskModal(false)
+            setShowAddTaskModal(false)
         }
     }
 
@@ -166,7 +169,7 @@ export default function Index() {
             description: addTaskObj.description
         }]
         setTasks(pushArray.concat(tasks))
-        setshowAddTaskModal(false)
+        setShowAddTaskModal(false)
         addTaskObj = {
             date: format(new Date, 'yyyy-MM-dd'),
             type: 'NR',
@@ -175,7 +178,8 @@ export default function Index() {
     }
 
     function viewTask(task) {
-        console.log(task)
+        setTask(JSON.parse(JSON.stringify(task)))
+        setShowViewTaskModal(true)
     }
 
     useEffect(() => {
@@ -198,7 +202,7 @@ export default function Index() {
             <main>
                 <div className="nav-logo">Logo</div>
                 <div className="nav-area d-f flex-jc-sb">
-                    <a className="c-i" href="#" onClick={(e) => { e.preventDefault(); setshowAddTaskModal(true) }}>+ Add task</a>
+                    <a className="c-i" href="#" onClick={(e) => { e.preventDefault(); setShowAddTaskModal(true) }}>+ Add task</a>
                     <div>
                         Tasks
                         <select className="ml-0_25em">
@@ -259,7 +263,7 @@ export default function Index() {
                         }
                         </tbody>
                     </table>
-                    <Modal showModal={showAddTaskModal} hideModal={() => setshowAddTaskModal(false)}>
+                    <Modal showModal={showAddTaskModal} hideModal={() => setShowAddTaskModal(false)}>
                         <form onSubmit={addTask} style={{ width: '30vw' }}>
                             <div className="d-f">
                                 <div>
@@ -288,10 +292,64 @@ export default function Index() {
                             </div>
                             <div className="mt-1em">
                                 <button>Add Task</button>
-                                <button className="ml-1em" type="button" onClick={() => setshowAddTaskModal(false)}>Cancel</button>
+                                <button className="ml-1em" type="button" onClick={() => setShowAddTaskModal(false)}>Cancel</button>
                             </div>
                         </form>
                     </Modal>
+                    { task &&
+                        <Modal showModal={showViewTaskModal} hideModal={() => setShowViewTaskModal(false)}>
+                            <div style={{ width: '40vw' }}>
+                                <div className="d-f">
+                                    <div>
+                                        <div>Date</div>
+                                        <div className="mt-0_25em">{ format(parseISO(task.date), displayDateFormat) }</div>
+                                    </div>
+                                    <div className="ml-1em">
+                                        <div>Type</div>
+                                        <div className="mt-0_25em">{ task.type }</div>
+                                    </div>
+                                </div>
+                                <div className="mt-1em">
+                                    <div>Description</div>
+                                    <div className="mt-0_25em">{ task.description }</div>
+                                </div>
+                                <div className="mt-1em">
+                                    <div className="tabs">
+                                        <div className={ activeTab === 'comments' ? 'active': null} onClick={() => setActiveTab('comments') }>Comments (0)</div>
+                                        <div className={ activeTab === 'files' ? 'active': null} onClick={() => setActiveTab('files') }>Files (0)</div>
+                                        <div className={ activeTab === 'assigned' ? 'active': null} onClick={() => setActiveTab('assigned') }>Assigned (0)</div>
+                                        <div className={ activeTab === 'time-spent' ? 'active': null} onClick={() => setActiveTab('time-spent') }>Time Spent (0 / 0:00)</div>
+                                    </div>
+                                    <div className="tabs-content">
+                                        {
+                                            activeTab === 'comments' &&
+                                                <div>
+                                                    Comments
+                                                </div>
+                                        }
+                                        {
+                                            activeTab === 'files' &&
+                                                <div>
+                                                    Files
+                                                </div>
+                                        }
+                                        {
+                                            activeTab === 'assigned' &&
+                                                <div>
+                                                    Assigned
+                                                </div>
+                                        }
+                                        {
+                                            activeTab === 'time-spent' &&
+                                                <div>
+                                                    Time Spent
+                                                </div>
+                                        }
+                                    </div>
+                                </div>
+                            </div>
+                        </Modal>
+                    }
                 </div>
             </main>
         </div>
