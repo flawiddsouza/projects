cd64cb3ddacb67f71bfa179fc7bb5efb7f45d3a2
Flawid D'Souza <me@flawiddsouza.com>
2019-09-20 22:06:44 +0530

    Adds Task View > Comments > Comment Edit & Delete

--- components/TaskView.js
+++ components/TaskView.js
@@ -22,6 +22,8 @@ export default function TaskView({ task }) {
     const [ timeSpendUpdate, setTimeSpendUpdate ] = useState(null)
     const [ updateTaskColumn, setUpdateTaskColumn ] = useState(null)
     const [ updateTaskColumnData, setUpdateTaskColumnData ] = useState(null)
+    const [ updateCommentId, setUpdateCommentId ] = useState(null)
+    const [ updateCommentData, setUpdateCommentData ] = useState(null)
 
     const commentsContainer = React.createRef()
 
@@ -236,7 +238,7 @@ export default function TaskView({ task }) {
                     <Fragment>
                         <div>Change Date</div>
                         <div className="mt-0_5em">
-                            <input type="date" value={updateTaskColumnData} onChange={e => setUpdateTaskColumnData(e.target.value)} autoFocus className="w-100p"></input>
+                            <input type="date" value={updateTaskColumnData} onChange={e => setUpdateTaskColumnData(e.target.value)} autoFocus className="w-100p" required></input>
                         </div>
                         <div className="mt-1em ta-r">
                             <button>Update</button>
@@ -279,7 +281,7 @@ export default function TaskView({ task }) {
                     <Fragment>
                         <div>Change Title</div>
                         <div className="mt-0_5em">
-                            <input type="text" value={updateTaskColumnData} onChange={e => setUpdateTaskColumnData(e.target.value)} autoFocus className="w-100p"></input>
+                            <input type="text" value={updateTaskColumnData} onChange={e => setUpdateTaskColumnData(e.target.value)} autoFocus className="w-100p" required></input>
                         </div>
                         <div className="mt-1em ta-r">
                             <button>Update</button>
@@ -289,6 +291,37 @@ export default function TaskView({ task }) {
                 </form>
             </Modal>
         )
+    }
+
+    function startCommentUpdate(e, comment) {
+        e.preventDefault()
+        setUpdateCommentData(comment.comment)
+        setUpdateCommentId(comment.id)
+    }
+
+    function updateComment(e) {
+        e.preventDefault()
+        // updateCommentId
+        // updateCommentData
+        cancelUpdateComment()
+    }
+
+    function cancelUpdateComment() {
+        setUpdateCommentData(null)
+        setUpdateCommentId(null)
+    }
+
+    function removeComment(e, comment) {
+        e.preventDefault()
+        if(confirm('Are you sure you want to delete this comment?')) {
+            setComments(comments.filter(item => item.id !== comment.id))
+        }
+    }
+
+    function setFocusToEndOfInput(e) {
+        let val = e.target.value
+        e.target.value = ''
+        e.target.value = val
     }
 
     return (
@@ -329,10 +362,10 @@ export default function TaskView({ task }) {
                                             <div className="label d-f flex-jc-sb">
                                                 <div>{commentItem.user}</div>
                                                 <div className="mr-0_5em hover-show-child">
-                                                    <a href="#">
+                                                    <a href="#" onClick={e => startCommentUpdate(e, commentItem)}>
                                                         <img src="/static/assets/pencil.svg" style={{ width: '15px', height: '15px'  }}></img>
                                                     </a>
-                                                    <a href="#" className="ml-0_5em">
+                                                    <a href="#" className="ml-0_5em" onClick={e => removeComment(e, commentItem)}>
                                                         <img src="/static/assets/delete.svg" style={{ width: '15px', height: '15px'  }}></img>
                                                     </a>
                                                 </div>
@@ -481,6 +514,17 @@ export default function TaskView({ task }) {
                 </div>
             </div>
             <UpdateTaskModal/>
+            <Modal showModal={updateCommentId !== null} hideModal={() => cancelUpdateComment()}>
+                <form onSubmit={updateComment} style={{ width: '50em' }}>
+                    <div>Edit Comment</div>
+                    <div className="mt-0_5em">
+                        <textarea value={updateCommentData} onChange={e => setUpdateCommentData(e.target.value)} autoFocus className="w-100p" style={{ height: '3.5em' }} onFocus={setFocusToEndOfInput} required></textarea>
+                    </div>
+                    <div className="mt-1em ta-r">
+                        <button>Update</button>
+                    </div>
+                </form>
+            </Modal>
         </div>
     )
 }
