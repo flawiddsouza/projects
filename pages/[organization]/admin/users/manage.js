e348a6830a518901caa6deb0faee70877bacf238
Flawid D'Souza <me@flawiddsouza.com>
2019-10-02 23:08:36 +0530

    Implements Organization > Admin Panel > Users > Manage > Add + remove members

--- pages/[organization]/admin/users/manage.js
+++ pages/[organization]/admin/users/manage.js
@@ -1,19 +1,117 @@
 import { useState, useEffect } from 'react'
 import Container from '../_container'
 import { useRouter } from 'next/router'
+import AsyncSelect from 'react-select/async'
+import api from 'Libs/esm/api'
 
 function Manage() {
 
-    const [ organizationUsers, setOrganizationUsers ] = useState([])
+    const [ organizationRoles, setOrganizationRoles ] = useState([])
+    const [ organizationMembers, setOrganizationMembers ] = useState([])
+    const [ selectedUserId, setSelectedUserId ] = useState(undefined)
+    const [ selectedOrganizationRoleId, setSelectedOrganizationRoleId ] = useState('')
     const router = useRouter()
     const { organization } = router.query
 
+    async function fetchMatchingUsers(inputValue) {
+        return await api.get(`${organization}/admin/matching-users?name=${inputValue}`).json()
+    }
+
+    function addMemberToOrganization(e) {
+        e.preventDefault()
+        api.post(`${organization}/admin/member`, {
+            json: {
+                user_id: selectedUserId.value,
+                organization_role_id: selectedOrganizationRoleId
+            }
+        }).then(() => {
+            fetchOrganizationMembers()
+        })
+        setSelectedUserId('')
+        setSelectedOrganizationRoleId('')
+    }
+
+    function showEditModalFunction(e, organizationUser) {
+        e.preventDefault()
+    }
+
+    async function deleteItem(e, id) {
+        e.preventDefault()
+        if(confirm('Are you sure you want remove this member from your organization?')) {
+            await api.delete(`${organization}/admin/member/${id}`)
+            fetchOrganizationMembers()
+        }
+    }
+
+    async function fetchOrganizationRoles() {
+        const organizationRoles = await api.get(`${organization}/admin/roles`).json()
+        setOrganizationRoles(organizationRoles)
+    }
+
+    async function fetchOrganizationMembers() {
+        const organizationMembers = await api.get(`${organization}/admin/members`).json()
+        setOrganizationMembers(organizationMembers)
+    }
+
     useEffect(() => {
+        fetchOrganizationMembers()
+        fetchOrganizationRoles()
     }, [])
 
     return (
         <Container>
-            Manage Organizations Users
+            <form onSubmit={addMemberToOrganization} className="d-f mb-1em">
+                <div style={{ width: '20em' }}>
+                    <AsyncSelect defaultOptions loadOptions={fetchMatchingUsers} onChange={selectedItem => setSelectedUserId(selectedItem)} value={selectedUserId}>
+                    </AsyncSelect>
+                    <input
+                        tabIndex={-1}
+                        autoComplete="off"
+                        style={{ opacity: 0, height: 0, position: 'absolute' }}
+                        value={selectedUserId || ''}
+                        required={true}
+                        onChange={() => {}}
+                    />
+                </div>
+                <select className="ml-1em" required onChange={e => setSelectedOrganizationRoleId(e.target.value)} value={selectedOrganizationRoleId}>
+                    <option></option>
+                    {
+                        organizationRoles.map(organizationRole => (
+                            <option key={organizationRole.id} value={organizationRole.id}>{organizationRole.role}</option>
+                        ))
+                    }
+                </select>
+                <button className="ml-1em">Add User to Organization</button>
+            </form>
+            <table className="table table-width-auto">
+                <thead>
+                    <tr>
+                        <th>Member</th>
+                        <th>Role</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        organizationMembers.length > 0 ?
+                            organizationMembers.map(organizationUser =>
+                                <tr key={organizationUser.id}>
+                                    <td>{organizationUser.user}</td>
+                                    <td>{organizationUser.role}</td>
+                                    <td>
+                                        <a href="#" onClick={e => showEditModalFunction(e, organizationUser)}>Edit</a>
+                                    </td>
+                                    <td>
+                                        <a href="#" onClick={e => deleteItem(e, organizationUser.id)}>Remove</a>
+                                    </td>
+                                </tr>
+                            )
+                            :
+                            <tr>
+                                <td colSpan="100%" className="ta-c">No Records Found</td>
+                            </tr>
+                    }
+                </tbody>
+            </table>
         </Container>
     )
 }
--- routes/organization-admin.js
+++ routes/organization-admin.js
@@ -9,8 +9,44 @@ generateCRUD(router, 'statuses', 'task_statuses', ['status', 'sort_order'], { wh
 generateCRUD(router, 'types', 'task_types', ['type', 'sort_order'], { where: 'organization_id', equals: 'organizationId'})
 generateCRUD(router, 'roles', 'organization_roles', ['role'], { where: 'organization_id', equals: 'organizationId'})
 
-router.get('/users', async(req, res) => {
-    res.send([])
+router.get('/matching-users', async(req, res) => { // matching users excluding organization members
+    if(req.query.name) {
+        let matchingUsers = await dbQuery(`
+            SELECT users.id as value, users.name as label
+            FROM users
+            LEFT JOIN organization_members ON organization_members.user_id = users.id AND organization_members.organization_id = ?
+            WHERE (users.name LIKE ? OR users.email LIKE ?) AND organization_members.id IS NULL
+        `, [req.organizationId, `%${req.query.name}%`, `%${req.query.name}%`])
+        res.json(matchingUsers)
+    } else {
+        res.json([])
+    }
+})
+
+router.get('/members', async(req, res) => {
+    let organizationMembers = await dbQuery(`
+        SELECT organization_members.id, users.name as user, organization_roles.role
+        FROM organization_members
+        JOIN users ON users.id = organization_members.user_id
+        JOIN organization_roles ON organization_roles.id = organization_members.organization_role_id
+        WHERE organization_members.organization_id = ?
+    `, [req.organizationId])
+    res.json(organizationMembers)
+})
+
+router.post('/member', async(req, res) => {
+    let insertedRecord = await dbQuery(`
+        INSERT INTO organization_members(organization_id, user_id, organization_role_id) VALUES(?, ?, ?)
+    `, [req.organizationId, req.body.user_id, req.body.organization_role_id])
+    res.json({ status: 'success', data: { insertedId: insertedRecord } })
+})
+
+router.delete('/member/:id', async(req, res) => {
+    await dbQuery(`
+        DELETE FROM organization_members
+        WHERE id = ?
+    `, [req.params.id])
+    res.json({ status: 'success' })
 })
 
 module.exports = router
