477e9612881e55134b634d30d9e2948f22ec58f7
Flawid D'Souza <me@flawiddsouza.com>
2019-10-03 23:55:37 +0530

    Move users above projects in organization > admin panel

--- pages/[organization]/admin/_container.js
+++ pages/[organization]/admin/_container.js
@@ -15,7 +15,16 @@ export default function Container({ children, Nav=null }) {
                 <select className="v-h"></select>
             </Page.Nav>
             <Page.Sidebar>
-                <div className="fw-b">Projects</div>
+            <div className="fw-b">Users</div>
+                <div className="mt-0_5em">
+                    <Link href="/[organization]/admin/users/roles" as={`/${organization}/admin/users/roles`} activeClassName="td-n c-b">
+                        <a className="d-b mt-0_25em">Roles</a>
+                    </Link>
+                    <Link href="/[organization]/admin/users/manage" as={`/${organization}/admin/users/manage`} activeClassName="td-n c-b">
+                        <a className="d-b mt-0_25em">Manage</a>
+                    </Link>
+                </div>
+                <div className="fw-b mt-1em">Projects</div>
                 <div className="mt-0_5em">
                     <Link href="/[organization]/admin/projects/manage" as={`/${organization}/admin/projects/manage`} activeClassName="td-n c-b">
                         <a className="d-b mt-0_25em">Manage</a>
@@ -31,15 +40,6 @@ export default function Container({ children, Nav=null }) {
                     </Link>
                     <Link href="/[organization]/admin/tasks/types" as={`/${organization}/admin/tasks/types`} activeClassName="td-n c-b">
                         <a className="d-b mt-0_25em">Types</a>
-                    </Link>
-                </div>
-                <div className="fw-b mt-1em">Users</div>
-                <div className="mt-0_5em">
-                    <Link href="/[organization]/admin/users/roles" as={`/${organization}/admin/users/roles`} activeClassName="td-n c-b">
-                        <a className="d-b mt-0_25em">Roles</a>
-                    </Link>
-                    <Link href="/[organization]/admin/users/manage" as={`/${organization}/admin/users/manage`} activeClassName="td-n c-b">
-                        <a className="d-b mt-0_25em">Manage</a>
                     </Link>
                 </div>
             </Page.Sidebar>
