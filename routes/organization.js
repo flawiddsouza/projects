142bb3e19360ad9550500cbca061dee9b43f7d5e
Flawid D'Souza <me@flawiddsouza.com>
2019-10-07 14:46:29 +0530

    Sort project members by name instead of assignment, this makes it easier for scanning

--- routes/organization.js
+++ routes/organization.js
@@ -45,6 +45,7 @@ router.get('/:project/members', validateProject, async(req, res) => {
         JOIN organization_roles ON organization_roles.id = organization_members.organization_role_id
         JOIN users ON users.id = organization_members.user_id
         WHERE project_members.project_id = ?
+        ORDER BY users.name
     `, [req.authUserId, req.projectId])
     res.json(projectMembers)
 })
