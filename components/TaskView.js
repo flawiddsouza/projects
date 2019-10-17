1c7f00f04c83f175f33072568b354dab215fc4fa
Flawid D'Souza <me@flawiddsouza.com>
2019-10-17 19:00:04 +0530

    Tabs overflow fix for when there are too many checklists

--- components/TaskView.js
+++ components/TaskView.js
@@ -236,7 +236,7 @@ export default function TaskView({ task, taskStatuses, taskTypes, projectCategor
                 <div className="mt-0_25em" onClick={() => startTaskColumnUpdate('title', task.title)}>{ task.title.trim() ? task.title : 'UNTITLED' }</div>
             </div>
             <div className="mt-1em">
-                <div className="tabs">
+                <div className="tabs ox-a">
                     <div className={ activeTab === 'comments' ? 'active': null} onClick={() => setActiveTab('comments') }>Comments ({commentsCount})</div>
                     <div className={ activeTab === 'files' ? 'active': null} onClick={() => setActiveTab('files') }>Files ({filesCount})</div>
                     <div className={ activeTab === 'assigned' ? 'active': null} onClick={() => setActiveTab('assigned') }>Assigned ({assignedCount})</div>
