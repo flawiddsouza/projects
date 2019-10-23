ebaeade82a96568cd6bf366541332395d13f89d9
Flawid D'Souza <me@flawiddsouza.com>
2019-10-23 18:30:44 +0530

    Disable notify by email search box on taskCompleted in Task View > Comments

--- components/TaskViewComments.js
+++ components/TaskViewComments.js
@@ -220,6 +220,7 @@ export default function TaskViewComments({ taskId, setCommentsCount, tabsContent
                                                 setCommentNotifyList([])
                                             }
                                         }}
+                                        isDisabled={taskCompleted}
                                     />
                                 </div>
                             </div>
